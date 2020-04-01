e208dda22fb22ead89e9ae7dae06233d8ce8c576
hugolo27 <hugo33lopez@hotmail.com>
2020-04-01 16:47:29 -0400

    Ajustes para heroku

--- ferreteria/settings.py
+++ ferreteria/settings.py
@@ -30,9 +30,9 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = ['django.contrib.admin',
+INSTALLED_APPS = [
     'jet',
-
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
